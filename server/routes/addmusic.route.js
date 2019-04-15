801e7060cad65cbd001e6d5f792c32a325d3af56
Vivek Shah <shahvicky1992@gmail.com>
2019-04-15 17:47:27 -0500

    api documentation

--- server/routes/addmusic.route.js
+++ server/routes/addmusic.route.js
@@ -6,11 +6,55 @@ const passport =   require('passport');
 const router = express.Router();
 require('./../middleware/passport')(passport)
 
-/** GET /api/addmusic/search - Search for music */
+/**
+ * 
+ * @api {GET} /api/addmusic/search Search Music
+ * @apiName Search a music from last.fm service
+ * @apiGroup AddMusic
+ * @apiVersion  1.0.0
+ * 
+ * 
+ * @apiParam  {String} searckKey The keyword to search the track, album, artist
+ * 
+ * @apiSuccess (200) {boolean} success Boolean to inform if the API was success or errored
+ * @apiSuccess (200) {json} data tracks result as got from the last.fm api
+ * 
+ * @apiSuccessExample {type} Success-Response:
+ * {
+ *     success : true,
+ *     data: {tracks: { ... }}
+ * }
+ * 
+ * 
+ */
 router.route('/search/:searchKey')
   .get(passport.authenticate('jwt', {session:false}), addmusicCtrl.search);
 
-/** GET /api/addmusic/addToLib - add track to user library */
+/**
+ * 
+ * @api {GET} /api/addmusic/addToLib Add music to user library
+ * @apiName Add Music to user library
+ * @apiGroup AddMusic
+ * @apiVersion  1.0.0
+ * 
+ * 
+ * @apiParam  {String} trackId The mbid of the track as required by the last.fm api to get track details
+ * 
+ * @apiSuccess (200) {boolean} success Boolean to inform if the API was success or errored
+ * @apiSuccess (200) {String} message The info whether a track is added or not
+ * 
+ * @apiParamExample  {type} Request-Example:
+ * {
+ *     trackId : 060e38a6-9ad9-44d3-abc1-741a0bf633ca
+ * }
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ * {
+ *     success : true,
+ *     message: "Track added to user library"
+ * }
+ * 
+ */
 router.route('/addToLib/:trackId')
   .get(passport.authenticate('jwt', {session:false}), addmusicCtrl.create);
 
--- server/routes/dashboard.routes.js
+++ server/routes/dashboard.routes.js
@@ -4,7 +4,29 @@ const passport =   require('passport');
 const router = express.Router();
 require('./../middleware/passport')(passport)
 
-/** GET /api/dashboard/tracks - Get all tracks for a user */
+/**
+ * 
+ * @api {GET} /api/dashboard/tracks Dashboard Tracks
+ * @apiName Get user tracks on the dashboard
+ * @apiGroup dashboard
+ * @apiVersion  1.0.0
+ * 
+ * @apiSuccess (200) {boolean} success Boolean to inform if the API was success or errored
+ * @apiSuccess (200) {Array} data The array of tracks to display on dashboard
+ * 
+ * @apiPermission authenticated user
+ * @apiHeader {String} id-token The unique jwt token
+ * @apiHeaderExample  {json} Header-Example:
+ * { 
+ *  "Authorization": Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJpYXQiOjE1NTUzNjUwNTQsImV4cCI6MTU1NTM3NTA1NH0.Wu-6yZiFzizAM5ZzvKvhSWSaD4900LvAOWKgOBKXtbM
+ * }
+ * 
+ * @apiSuccessExample {type} Success-Response:
+ * {
+ *     data: [{trkTrackId: "060e38a6-9ad9-44d3-abc1-741a0bf633ca", trkUserId: 5, trkTrackName: "Yanar?m",…},…],
+ *     success: true
+ * }
+ */
 router.route('/tracks')
   .get(passport.authenticate('jwt', {session:false}), dashboardCtrl.getTracks);
 
